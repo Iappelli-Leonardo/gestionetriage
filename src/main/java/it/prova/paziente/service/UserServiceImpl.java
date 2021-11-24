9cdda30c84080b82f46615443415d9f3b9ed686e
leoIapp <leoiappelli01@gmail.com>
2021-11-24 10:02:23 +0100

    impl DottoreService

--- src/main/java/it/prova/paziente/service/UserServiceImpl.java
+++ src/main/java/it/prova/paziente/service/UserServiceImpl.java
@@ -1,22 +1,23 @@
 package it.prova.paziente.service;
 
-import java.awt.print.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.transaction.Transactional;
 import javax.persistence.criteria.Predicate;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import it.prova.paziente.exception.UserNotFoundException;
 import it.prova.paziente.model.User;
 import it.prova.paziente.repository.UserRepository;
-import net.bytebuddy.asm.Advice.OffsetMapping.Sort;
 
 @Service
 public class UserServiceImpl implements UserService{
